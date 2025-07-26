286eaeaf3501ab43d4903aa7090896e93823f60e
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-07-26 23:42:19 +0200

    cleaned stuff up

--- common/src/main/java/net/mehvahdjukaar/feudalist/FeudalistMod.java
+++ common/src/main/java/net/mehvahdjukaar/feudalist/FeudalistMod.java
@@ -4,6 +4,7 @@ package net.mehvahdjukaar.feudalist;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.PushReaction;
 import org.apache.logging.log4j.LogManager;
@@ -14,20 +15,17 @@ import java.util.function.Supplier;
 
 public class FeudalistMod {
 
-    public static final String MOD_ID = "map_atlases";
-    public static final Logger LOGGER = LogManager.getLogger("Map Atlases");
+    public static final String MOD_ID = "feudalist";
+    public static final Logger LOGGER = LogManager.getLogger("Feudalist");
 
     public static ResourceLocation res(String path) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 
-    public static final Supplier<Block> DUMB_BLOCK = RegHelper.registerBlockWithItem(
-            res("empty_maps"), () -> new Block(
-                    BlockBehaviour.Properties.of()
-                            .noCollission()
-                            .instabreak()
-                            .noOcclusion()
-                            .pushReaction(PushReaction.DESTROY)
+    public static final Supplier<Block> FANCY_STONE = RegHelper.registerBlockWithItem(
+            res("fancy_stone"), () -> new Block(
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                            .destroyTime(2)
             )
     );
 
