a44941c5a1e51b50b512c69e6b480a5d186fa81f
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-07-27 00:16:36 +0200

    aa

--- common/src/main/java/net/mehvahdjukaar/feudalist/FeudalistMod.java
+++ common/src/main/java/net/mehvahdjukaar/feudalist/FeudalistMod.java
@@ -1,15 +1,19 @@
 package net.mehvahdjukaar.feudalist;
 
 
+import net.mehvahdjukaar.moonlight.api.misc.RegSupplier;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.PushReaction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 
@@ -22,15 +26,42 @@ public class FeudalistMod {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 
-    public static final Supplier<Block> FANCY_STONE = RegHelper.registerBlockWithItem(
-            res("fancy_stone"), () -> new Block(
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
-                            .destroyTime(2)
+    private static final List<Supplier<Block>> BLOCKS = new ArrayList<>();
+
+    // add blocks below here
+
+    public static final Supplier<Block> FANCY_STONE = regBlock("fancy_stone",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                    .destroyTime(2)
             )
     );
 
 
+
+    // end blocks
+
+
+    public static final RegSupplier<CreativeModeTab> TAB = RegHelper.registerCreativeModeTab(
+            res("feudalist_tab"), b -> {
+                b.icon(() -> FANCY_STONE.get().asItem().getDefaultInstance());
+            }
+    );
+
+    private static RegSupplier<Block> regBlock(String id, Supplier<Block> blockSupplier) {
+        var s = RegHelper.registerBlockWithItem(
+                res(id), blockSupplier);
+        BLOCKS.add(s);
+        return s;
+    }
+
     public static void init() {
+        RegHelper.addItemsToTabsRegistration(itemToTabEvent -> {
+            itemToTabEvent.add((ResourceKey<CreativeModeTab>) TAB.getKey(),
+                    BLOCKS.stream()
+                            .map(Supplier::get)
+                            .toArray(Block[]::new));
+
+        });
 
     }
 
